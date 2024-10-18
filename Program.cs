9778b581848e01789d3a5ad81dd2d4e0cfce0028
BRicsi <barnusrichi@gmail.com>
2024-10-18 10:38:34 +0200

    :))))))))

--- Program.cs
+++ Program.cs
@@ -10,6 +10,38 @@ namespace harmadikFeladat
     {
         static void Main(string[] args)
         {
+            int[] homersekletek = new int[7];
+            bool voltFagypontAlatti = false;  
+
+            for (int i = 0; i < 7; i++)
+            {
+                Console.Write("Add meg a(z) {0}. nap hőmérsékletét: ", i + 1);
+                homersekletek[i] = int.Parse(Console.ReadLine());
+
+                if (homersekletek[i] < 0)
+                {
+                    voltFagypontAlatti = true;
+                }
+            }
+
+            double atlagHomerseklet = homersekletek.Average();
+
+            int legmagasabbHomerseklet = homersekletek.Max();
+
+            int legalacsonyabbHomerseklet = homersekletek.Min();
+
+            Console.WriteLine("A hét átlaghőmérséklete: {0:F2}°C", atlagHomerseklet);
+            Console.WriteLine("A legmelegebb nap hőmérséklete: {0}°C", legmagasabbHomerseklet);
+            Console.WriteLine("A leghidegebb nap hőmérséklete: {0}°C", legalacsonyabbHomerseklet);
+
+            if (voltFagypontAlatti)
+            {
+                Console.WriteLine("Fagypont alatti nap is volt!");
+            }
+            else
+            {
+                Console.WriteLine("Nem volt fagypont alatti nap.");
+            }
         }
     }
 }
